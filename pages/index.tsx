50ed3f4d7571a45d97b9e78943f1f24d32d527b3
Jack <jgirelma@andrew.cmu.edu>
2020-08-09 16:45:07 -0400

    no examples component in master

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Navbar from "../Components/Navbar";
 import { useState } from "react";
 import Grid from "../Components/Grid";
 import GridControllsContext from "../Contexts/GridControllsContext";
-import Examples from "../Components/Examples";
 
 export default function Home() {
   const [gridSize, setGridSize] = useState(30);
@@ -16,7 +15,6 @@ export default function Home() {
       <Navbar />
       <div className="mt-4 lg:grid grid-cols-3">
         <Grid />
-        {/* <Examples /> */}
       </div>
     </GridControllsContext.Provider>
   );
