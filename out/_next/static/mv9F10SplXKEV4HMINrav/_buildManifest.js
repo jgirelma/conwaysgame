e9a5fedf621184ecf746067cdb0017dfe7e2cfc3
Jack <jgirelma@andrew.cmu.edu>
2020-08-09 22:32:01 -0400

    Remove ignored files

--- out/_next/static/mv9F10SplXKEV4HMINrav/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-5be8846ce1a512df20a9.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-767ed047ea24eb9c87d7.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
